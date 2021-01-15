fd0a492ac0ec0d03f832474c88cf70ccd1498711
Alex Rinaldi <s1096206@studenti.univpm.it>
2021-01-15 21:41:04 +0100

    inserita la superclasse stats

--- progettoOOP/src/main/java/univpm/progettoOOP/stats/Stats.java
+++ progettoOOP/src/main/java/univpm/progettoOOP/stats/Stats.java
@@ -1,5 +1,37 @@
 package univpm.progettoOOP.stats;
 
-public interface Stats {
-	//getStats(Domain[] filteredList){}
-}
+import univpm.progettoOOP.model.Domain;
+
+import org.json.simple.JSONObject;
+
+import java.util.HashSet;
+
+/**
+ * <b>Superclasse</b> relativa al calcolo delle statistiche
+ * @author Meloni Matteo
+ * @author Rinaldi Alex
+ * @version 1.0
+ */
+
+public abstract class Stats {
+	
+	/**
+	 * <b>Hashset</b> contenente i domini sui quali verranno elaborate le statistiche
+	 */
+	
+	HashSet<Domain> domainList;
+	
+	/**
+     * <b>Costruttore</b> della classe
+     * @param domainlist: Hashset dei domini sui quali elaborare le statistiche
+     */
+    public Stats(HashSet<Domain> domainList) {
+    
+    }
+    /**
+     * Metodo astratto per calcolare le statistiche
+     */
+    public abstract JSONObject calculateStat();
+    }
+    
+
